48343d3483d4377080c5520e353517e04969c88b
pujan01 <thapapujan2@gmail.com>
2018-10-27 16:53:32 -0400

    Step 2 Dismiss creation screen

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Pujan Thapa on 10/27/18.
+//  Copyright © 2018 Sattique. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func DidTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
